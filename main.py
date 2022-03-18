5daf26e2d62a82e12b77a40e1e10863d1f42fb85
Abdelrahman Atef <abdelrahmanatef38@gmail.com>
2022-03-19 01:16:44 +0200

    A College project (Introduction To Bioinformatics Course) LOCAL ALIGNMENT : to perform local alignment on two DNA or Protein Sequences. GLOBAL ALIGNMENT : to perform global alignment on two DNA or Protein Sequences

---
+++ main.py
@@ -0,0 +1,412 @@
+blosum62 = {
+        ('W', 'F'): 1, ('L', 'R'): -2, ('S', 'P'): -1, ('V', 'T'): 0,
+        ('Q', 'Q'): 5, ('N', 'A'): -2, ('Z', 'Y'): -2, ('W', 'R'): -3,
+        ('Q', 'A'): -1, ('S', 'D'): 0, ('H', 'H'): 8, ('S', 'H'): -1,
+        ('H', 'D'): -1, ('L', 'N'): -3, ('W', 'A'): -3, ('Y', 'M'): -1,
+        ('G', 'R'): -2, ('Y', 'I'): -1, ('Y', 'E'): -2, ('B', 'Y'): -3,
+        ('Y', 'A'): -2, ('V', 'D'): -3, ('B', 'S'): 0, ('Y', 'Y'): 7,
+        ('G', 'N'): 0, ('E', 'C'): -4, ('Y', 'Q'): -1, ('Z', 'Z'): 4,
+        ('V', 'A'): 0, ('C', 'C'): 9, ('M', 'R'): -1, ('V', 'E'): -2,
+        ('T', 'N'): 0, ('P', 'P'): 7, ('V', 'I'): 3, ('V', 'S'): -2,
+        ('Z', 'P'): -1, ('V', 'M'): 1, ('T', 'F'): -2, ('V', 'Q'): -2,
+        ('K', 'K'): 5, ('P', 'D'): -1, ('I', 'H'): -3, ('I', 'D'): -3,
+        ('T', 'R'): -1, ('P', 'L'): -3, ('K', 'G'): -2, ('M', 'N'): -2,
+        ('P', 'H'): -2, ('F', 'Q'): -3, ('Z', 'G'): -2, ('X', 'L'): -1,
+        ('T', 'M'): -1, ('Z', 'C'): -3, ('X', 'H'): -1, ('D', 'R'): -2,
+        ('B', 'W'): -4, ('X', 'D'): -1, ('Z', 'K'): 1, ('F', 'A'): -2,
+        ('Z', 'W'): -3, ('F', 'E'): -3, ('D', 'N'): 1, ('B', 'K'): 0,
+        ('X', 'X'): -1, ('F', 'I'): 0, ('B', 'G'): -1, ('X', 'T'): 0,
+        ('F', 'M'): 0, ('B', 'C'): -3, ('Z', 'I'): -3, ('Z', 'V'): -2,
+        ('S', 'S'): 4, ('L', 'Q'): -2, ('W', 'E'): -3, ('Q', 'R'): 1,
+        ('N', 'N'): 6, ('W', 'M'): -1, ('Q', 'C'): -3, ('W', 'I'): -3,
+        ('S', 'C'): -1, ('L', 'A'): -1, ('S', 'G'): 0, ('L', 'E'): -3,
+        ('W', 'Q'): -2, ('H', 'G'): -2, ('S', 'K'): 0, ('Q', 'N'): 0,
+        ('N', 'R'): 0, ('H', 'C'): -3, ('Y', 'N'): -2, ('G', 'Q'): -2,
+        ('Y', 'F'): 3, ('C', 'A'): 0, ('V', 'L'): 1, ('G', 'E'): -2,
+        ('G', 'A'): 0, ('K', 'R'): 2, ('E', 'D'): 2, ('Y', 'R'): -2,
+        ('M', 'Q'): 0, ('T', 'I'): -1, ('C', 'D'): -3, ('V', 'F'): -1,
+        ('T', 'A'): 0, ('T', 'P'): -1, ('B', 'P'): -2, ('T', 'E'): -1,
+        ('V', 'N'): -3, ('P', 'G'): -2, ('M', 'A'): -1, ('K', 'H'): -1,
+        ('V', 'R'): -3, ('P', 'C'): -3, ('M', 'E'): -2, ('K', 'L'): -2,
+        ('V', 'V'): 4, ('M', 'I'): 1, ('T', 'Q'): -1, ('I', 'G'): -4,
+        ('P', 'K'): -1, ('M', 'M'): 5, ('K', 'D'): -1, ('I', 'C'): -1,
+        ('Z', 'D'): 1, ('F', 'R'): -3, ('X', 'K'): -1, ('Q', 'D'): 0,
+        ('X', 'G'): -1, ('Z', 'L'): -3, ('X', 'C'): -2, ('Z', 'H'): 0,
+        ('B', 'L'): -4, ('B', 'H'): 0, ('F', 'F'): 6, ('X', 'W'): -2,
+        ('B', 'D'): 4, ('D', 'A'): -2, ('S', 'L'): -2, ('X', 'S'): 0,
+        ('F', 'N'): -3, ('S', 'R'): -1, ('W', 'D'): -4, ('V', 'Y'): -1,
+        ('W', 'L'): -2, ('H', 'R'): 0, ('W', 'H'): -2, ('H', 'N'): 1,
+        ('W', 'T'): -2, ('T', 'T'): 5, ('S', 'F'): -2, ('W', 'P'): -4,
+        ('L', 'D'): -4, ('B', 'I'): -3, ('L', 'H'): -3, ('S', 'N'): 1,
+        ('B', 'T'): -1, ('L', 'L'): 4, ('Y', 'K'): -2, ('E', 'Q'): 2,
+        ('Y', 'G'): -3, ('Z', 'S'): 0, ('Y', 'C'): -2, ('G', 'D'): -1,
+        ('B', 'V'): -3, ('E', 'A'): -1, ('Y', 'W'): 2, ('E', 'E'): 5,
+        ('Y', 'S'): -2, ('C', 'N'): -3, ('V', 'C'): -1, ('T', 'H'): -2,
+        ('P', 'R'): -2, ('V', 'G'): -3, ('T', 'L'): -1, ('V', 'K'): -2,
+        ('K', 'Q'): 1, ('R', 'A'): -1, ('I', 'R'): -3, ('T', 'D'): -1,
+        ('P', 'F'): -4, ('I', 'N'): -3, ('K', 'I'): -3, ('M', 'D'): -3,
+        ('V', 'W'): -3, ('W', 'W'): 11, ('M', 'H'): -2, ('P', 'N'): -2,
+        ('K', 'A'): -1, ('M', 'L'): 2, ('K', 'E'): 1, ('Z', 'E'): 4,
+        ('X', 'N'): -1, ('Z', 'A'): -1, ('Z', 'M'): -1, ('X', 'F'): -1,
+        ('K', 'C'): -3, ('B', 'Q'): 0, ('X', 'B'): -1, ('B', 'M'): -3,
+        ('F', 'C'): -2, ('Z', 'Q'): 3, ('X', 'Z'): -1, ('F', 'G'): -3,
+        ('B', 'E'): 1, ('X', 'V'): -1, ('F', 'K'): -3, ('B', 'A'): -2,
+        ('X', 'R'): -1, ('D', 'D'): 6, ('W', 'G'): -2, ('Z', 'F'): -3,
+        ('S', 'Q'): 0, ('W', 'C'): -2, ('W', 'K'): -3, ('H', 'Q'): 0,
+        ('L', 'C'): -1, ('W', 'N'): -4, ('S', 'A'): 1, ('L', 'G'): -4,
+        ('W', 'S'): -3, ('S', 'E'): 0, ('H', 'E'): 0, ('S', 'I'): -2,
+        ('H', 'A'): -2, ('S', 'M'): -1, ('Y', 'L'): -1, ('Y', 'H'): 2,
+        ('Y', 'D'): -3, ('E', 'R'): 0, ('X', 'P'): -2, ('G', 'G'): 6,
+        ('G', 'C'): -3, ('E', 'N'): 0, ('Y', 'T'): -2, ('Y', 'P'): -3,
+        ('T', 'K'): -1, ('A', 'A'): 4, ('P', 'Q'): -1, ('T', 'C'): -1,
+        ('V', 'H'): -3, ('T', 'G'): -2, ('I', 'Q'): -3, ('Z', 'T'): -1,
+        ('C', 'R'): -3, ('V', 'P'): -2, ('P', 'E'): -1, ('M', 'C'): -1,
+        ('K', 'N'): 0, ('I', 'I'): 4, ('P', 'A'): -1, ('M', 'G'): -3,
+        ('T', 'S'): 1, ('I', 'E'): -3, ('P', 'M'): -2, ('M', 'K'): -1,
+        ('I', 'A'): -1, ('P', 'I'): -3, ('R', 'R'): 5, ('X', 'M'): -1,
+        ('L', 'I'): 2, ('X', 'I'): -1, ('Z', 'B'): 1, ('X', 'E'): -1,
+        ('Z', 'N'): 0, ('X', 'A'): 0, ('B', 'R'): -1, ('B', 'N'): 3,
+        ('F', 'D'): -3, ('X', 'Y'): -1, ('Z', 'R'): 0, ('F', 'H'): -1,
+        ('B', 'F'): -3, ('F', 'L'): 0, ('X', 'Q'): -1, ('B', 'B'): 4
+    }
+gap = -1
+def check(x,y):
+    if (x,y) in blosum62:
+        score = blosum62[(x,y)]
+    else:
+        score = blosum62[(y,x)]
+    return score
+def dna_global(seq1,seq2):
+    m = len(seq1)
+    n = len(seq2)
+    L = [[0 for x in range(m + 1)] for y in range(n + 1)]
+    for i in range(n + 1):
+        L[i][0] = -i
+    for j in range(m + 1):
+        L[0][j] = -j
+    for i in range(1, n + 1, 1):
+        for j in range(1, m + 1, 1):
+            if seq1[j - 1] == seq2[i - 1]:
+                score = 1
+            else:
+                score = -1
+            L[i][j] = max(L[i - 1][j] - 1, L[i][j - 1] - 1, L[i - 1][j - 1] + score)
+    print(L[n][m])
+   # for line in L:
+   #     print(line)
+    dnaFirst =""
+    dnaSecond=""
+    dnaMatch=""
+    i = n
+    j = m
+    while i > 0 and j > 0:
+        up = L[i-1][j]-1
+        left=L[i][j-1]-1
+        if  seq1[j-1]==seq2[i-1]:
+            score=1
+        else :
+            score=-1
+        diagonal = L[i-1][j-1]+score
+        if L[i][j]==diagonal:
+            dnaFirst+=seq1[j-1]
+            dnaSecond+=seq2[i-1]
+            if score==1:
+                dnaMatch+="|"
+            else:
+                dnaMatch+=" "
+            i-=1
+            j-=1
+        elif L[i][j] == up:
+            dnaFirst += "-"
+            dnaMatch += " "
+            dnaSecond += seq2[i - 1]
+            i -= 1
+        else:
+            dnaFirst += seq1[j - 1]
+            dnaSecond += "-"
+            dnaMatch += " "
+            j -= 1
+    while (i > 0):
+        dnaFirst += "-"
+        dnaSecond += seq2[i - 1]
+        dnaMatch += " "
+        i -= 1
+    while (j > 0):
+        dnaFirst += seq1[j - 1]
+        dnaSecond += "-"
+        dnaMatch += " "
+        j -= 1
+        dnaFirst = dnaFirst[::-1]
+        dnaMatch = dnaMatch[::-1]
+        dnaSecond = dnaSecond[::-1]
+    print(dnaFirst)
+    print(dnaMatch)
+    print(dnaSecond)
+
+def protein_global(seq1,seq2):
+    m = len(seq1)
+    n = len(seq2)
+    L = [[0 for x in range(m + 1)] for y in range(n + 1)]
+
+    for i in range(n + 1):
+        L[i][0] = -i
+    for j in range(m + 1):
+        L[0][j] = -j
+    for i in range(1, n + 1, 1):
+        for j in range(1, m + 1, 1):
+             L[i][j] = max(L[i - 1][j] +gap, L[i][j - 1] +gap, L[i - 1][j - 1] + check(seq1[i-1],seq2[j-1]))
+
+    for line in L:
+        print(line)
+    print("Alignment score is : " , L[n][m])
+    proFirst = ""
+    proSecond = ""
+    proMatch = ""
+    i = n
+    j = m
+    while i > 0 and j > 0:
+        up = L[i - 1][j] - 1
+        left = L[i][j - 1] - 1
+        diagonal = max(L[i - 1][j] +gap, L[i][j - 1] +gap, L[i - 1][j - 1] + check(seq1[i-1],seq2[j-1]))
+        if L[i][j] == diagonal:
+            proFirst += seq1[j - 1]
+            proSecond += seq2[i - 1]
+            if seq1[j - 1] == seq2[i - 1]:
+                proMatch += "|"
+            else:
+                proMatch += " "
+            i -= 1
+            j -= 1
+        elif L[i][j] == up:
+            proFirst += "-"
+            proMatch += " "
+            proSecond += seq2[i - 1]
+            i -= 1
+        else:
+            proFirst += seq1[j - 1]
+            proSecond += "-"
+            proMatch += " "
+            j -= 1
+    while (i > 0):
+        proFirst += "-"
+        proSecond += seq2[i - 1]
+        proMatch += " "
+        i -= 1
+    while (j > 0):
+        proFirst += seq1[j - 1]
+        proSecond += "-"
+        proMatch += " "
+        j -= 1
+        proFirst = proFirst[::-1]
+        proMatch = proMatch[::-1]
+        proSecond = proSecond[::-1]
+    print(proFirst)
+    print(proMatch)
+    print(proSecond)
+
+def dna_local(seq1,seq2):
+    m = len(seq1)
+    n = len(seq2)
+    L = [[0 for x in range(m + 1)] for y in range(n + 1)]
+    for i in range(n + 1):
+        L[i][0] = 0
+    for j in range(m + 1):
+        L[0][j] = 0
+    for i in range(1, n + 1, 1):
+        for j in range(1, m + 1, 1):
+            if seq1[j - 1] == seq2[i - 1]:
+                score = 1
+            else:
+                score = -1
+            val = max(L[i - 1][j] - 1, L[i][j - 1] - 1, L[i - 1][j - 1] + score)
+            if val< 0:
+                val= 0
+            else :
+                val=val
+            L[i][j] =val
+    print(L[n][m])
+    for line in L:
+        print(line)
+
+#getting the max cell
+    maximum_cell =0
+    x, y = -1, -1
+    for a in range(len(L)):
+        for b in range(len(L)):
+            if L[a][b] > maximum_cell:
+                maximum_cell = L[a][b]
+                x, y = a, b
+    print("Max score of the matrix : " ,maximum_cell)
+
+
+    dnaFirst = ""
+    dnaSecond = ""
+    dnaMatch = ""
+    i = x
+    j = y
+    while i > 0 and j > 0:
+        up = L[i - 1][j] - 1
+        left = L[i][j - 1] - 1
+        if seq1[j - 1] == seq2[i - 1]:
+            score = 1
+        else:
+            score = -1
+        diagonal = L[i - 1][j - 1] + score
+        if L[i][j] == diagonal:
+            dnaFirst += seq1[j - 1]
+            dnaSecond += seq2[i - 1]
+            if score == 1:
+                dnaMatch += "|"
+            else:
+                dnaMatch += " "
+            i -= 1
+            j -= 1
+        elif L[i][j] == up:
+            dnaFirst += "-"
+            dnaMatch += " "
+            dnaSecond += seq2[i - 1]
+            i -= 1
+        else:
+            dnaFirst += seq1[j - 1]
+            dnaSecond += "-"
+            dnaMatch += " "
+            j -= 1
+    while (i > 0):
+        dnaFirst += "-"
+        dnaSecond += seq2[i - 1]
+        dnaMatch += " "
+        i -= 1
+    while (j > 0):
+        dnaFirst += seq1[j - 1]
+        dnaSecond += "-"
+        dnaMatch += " "
+        j -= 1
+        dnaFirst = dnaFirst[::-1]
+        dnaMatch = dnaMatch[::-1]
+        dnaSecond = dnaSecond[::-1]
+
+    print(dnaFirst)
+    print(dnaMatch)
+    print(dnaSecond)
+
+def protein_local(seq1,seq2):
+    m = len(seq1)
+    n = len(seq2)
+    L = [[0 for x in range(m + 1)] for y in range(n + 1)]
+    for i in range(n + 1):
+        L[i][0] = 0
+    for j in range(m + 1):
+        L[0][j] = 0
+    for i in range(1, n + 1, 1):
+        for j in range(1, m + 1, 1):
+            val = max(L[i - 1][j] + gap, L[i][j - 1] + gap, L[i - 1][j - 1] + check(seq1[i - 1], seq2[j - 1]))
+            if val < 0:
+                val = 0
+            else:
+                val = val
+            L[i][j] = val
+    for line in L:
+        print(line)
+#getting the max cell
+    maximum_cell =0
+    x, y = -1, -1
+    for a in range(len(L)):
+        for b in range(len(L)):
+            if L[a][b] > maximum_cell:
+                maximum_cell = L[a][b]
+                x, y = a, b
+    print("Max score of the matrix : " ,maximum_cell)
+#TRACE BACK
+    proFirst = ""
+    proSecond = ""
+    proMatch = ""
+    i = x
+    j = y
+    while i > 0 and j > 0:
+        up = L[i - 1][j] - 1
+        left = L[i][j - 1] - 1
+        val = max(L[i - 1][j] + gap, L[i][j - 1] + gap, L[i - 1][j - 1] + check(seq1[i - 1], seq2[j - 1]))
+        if val < 0:
+            val = 0
+        else:
+            val = val
+        diagonal = val
+        #print(diagonal)
+        if L[i][j] == diagonal:
+            proFirst += seq1[j - 1]
+            proSecond += seq2[i - 1]
+            if seq1[j - 1] == seq2[i - 1]:
+                proMatch += "|"
+            else:
+                proMatch += " "
+            i -= 1
+            j -= 1
+        elif L[i][j] == up:
+            proFirst += "-"
+            proMatch += " "
+            proSecond += seq2[i - 1]
+            i -= 1
+        else:
+            proFirst += seq1[j - 1]
+            proSecond += "-"
+            proMatch += " "
+            j -= 1
+    while (i > 0):
+        proFirst += "-"
+        proSecond += seq2[i - 1]
+        proMatch += " "
+        i -= 1
+    while (j > 0):
+        proFirst += seq1[j - 1]
+        proSecond += "-"
+        proMatch += " "
+        j -= 1
+        proFirst = proFirst[::-1]
+        proMatch = proMatch[::-1]
+        proSecond = proSecond[::-1]
+
+    print(proFirst)
+    print(proMatch)
+    print(proSecond)
+
+def main():
+    print( "---  Welcome to alignment system  ---")
+    print(" Please  specify the type of alignment you want to use ")
+    print (" 1- Global alignment" "\r\n" " 2- Local alignment")
+    choice= input("your choice :")
+    if choice =='1':
+        print(" Global alignment" "\r\n" " Please  specify the type of molecule  you want to align ")
+        print(" 1- DNA" "\r\n" " 2- Protein ")
+        choice2 = input("your choice :")
+        if choice2 == '1':
+            print(" -DNA alinment- ")
+            seq1=input("Enter the first sequences : " )
+            seq2=input("Enter the second sequences : " )
+            dna_global(seq1,seq2)
+        elif choice2 == '2':
+            print(" -Protein alinment- ")
+            seq1 = input("Enter the first sequences : ")
+            seq2 = input("Enter the second sequences : ")
+            protein_global(seq1, seq2)
+        else:
+            print("invalid choice")
+    elif choice =='2':
+        print(" Local alignment" "\r\n" " Please  specify the type of molecule  you want to align ")
+        print(" 1- DNA" "\r\n" " 2- Protein ")
+        choice2 = input("your choice :")
+        if choice2 == '1':
+            print(" -DNA alinment- ")
+            seq1 = input("Enter the first sequences : ")
+            seq2 = input("Enter the second sequences : ")
+            dna_local(seq1,seq2)
+        elif choice2 == '2':
+            print(" -Protein alinment- ")
+            seq1 = input("Enter the first sequences : ")
+            seq2 = input("Enter the second sequences : ")
+            protein_local(seq1,seq2)
+        else:
+            print("invalid choice")
+    else:
+         print("invalid choice")
+
+main()
